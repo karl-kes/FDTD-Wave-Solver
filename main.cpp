eb6176ff41321632d79bdd7632c05ded62af30de
Karl Keshavarzi <kkeshavarzi@outlook.com>
2025-12-24 19:15:18 -0800

    Fixed energy drift calculation; previously only tracked LAST energy drift, not max

--- main.cpp
+++ main.cpp
@@ -20,22 +20,21 @@ int main() {
     std::filesystem::remove_all("output");
     std::filesystem::create_directories("output");
 
-    int constexpr elapsed_time{ 100 };
+    int constexpr elapsed_time{ 1000 };
     std::size_t constexpr Nx{ 50 }, Ny{ 50 }, Nz{ 50 };
-
-    auto start = std::chrono::high_resolution_clock::now();
 
     Grid grid{ Nx, Ny, Nz };
     grid.inject_source( 25, 25, 25, 100.0 );
 
     double initial_energy{ grid.total_energy() };
-    double max_energy_drift{};
+    double max_energy_drift{ initial_energy };
 
+    auto start = std::chrono::high_resolution_clock::now();
     for ( int t{}; t <= elapsed_time; ++t ) {
         grid.step();
 
-        // Max between current and initial.
-        max_energy_drift = std::max( grid.total_energy(), initial_energy );
+        // Max between current and previous max.
+        max_energy_drift = std::max( grid.total_energy(), max_energy_drift );
 
         if ( t % 10 == 0 ) {
             std::string t_sec{ std::to_string( t ) };
@@ -43,13 +42,14 @@ int main() {
             grid.magnitude_slice( 1, "output/B" + t_sec + ".csv", 'B' );
         }
     }
-    double drift{ 100 * ( max_energy_drift - initial_energy ) / initial_energy };
     auto end = std::chrono::high_resolution_clock::now();
-
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    double drift{ 100 * ( max_energy_drift - initial_energy ) / initial_energy };
+
     std::cout << "Physical Time Simulated: " << elapsed_time * grid.dt() << " s" << std::endl;
     std::cout << "Duration of Simulation: " << duration.count() << " ms" << std::endl;
-    std::cout << "Drift: " << drift << "%" << std::endl;
+    std::cout << "Max Energy Drift: " << drift << "%" << std::endl;
 
     return 0;
 }
